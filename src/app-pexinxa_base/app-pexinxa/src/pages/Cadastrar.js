99f515ac3300c1966606cdd2ca9a09dccd1a0480
Fernando Alves <90225435+fernends@users.noreply.github.com>
2023-05-03 21:14:59 -0300

    Update Cadastrar.js
    Inserindo o códigos desenvolvidos para a página cadastro

--- src/app-pexinxa_base/app-pexinxa/src/pages/Cadastrar.js
+++ src/app-pexinxa_base/app-pexinxa/src/pages/Cadastrar.js
@@ -1,10 +1,180 @@
-import { Text, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, ScrollView } from 'react-native';
+import { Appbar, TextInput, Button, Text } from 'react-native-paper';
 
-export default function Cadastrar({ navigation }) {
+const Cadastro = () => {
+  const [nome, setNome] = useState('');
+  const [doc, setDoc] = useState('');
+  const [mail, setMail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [cep, setCep] = useState('');
+  const [rua, setRua] = useState('');
+  const [numero, setNumero] = useState('');
+  const [complemento, setComplemento] = useState('');
+  const [bairro, setBairro] = useState('');
+  const [cidade, setCidade] = useState('');
+  const [estado, setEstado] = useState('');
+  const [telefone, setTelefone] = useState('');
+
+  const fetchAddress = async () => {
+    if (cep.length === 8) {
+      try {
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        const data = await response.json();
+        setRua(data.logradouro);
+        setBairro(data.bairro);
+        setCidade(data.localidade);
+        setEstado(data.uf);
+        console.log(response.ok);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
-    <>
-      <Text>Página de Cadastro</Text>
-      <Button title="Voltar" onPress={() => navigation.goBack()}></Button>
-    </>
+
+    <ScrollView style={styles.container}>
+      <Appbar.Header>
+        <Appbar.Content title="Página de Cadastro" />
+      </Appbar.Header>
+
+      <ScrollView style={styles.body}>
+      <Text style={styles.titulo}>Insira os seus dados</Text>
+      <Text style={styles.text}> Dados Pessoais </Text>
+        <TextInput
+          style={styles.input}
+          label="Nome completo"
+          value={nome}
+          onChangeText={(text) => setNome(text)}
+        />
+        <TextInput
+          style={styles.input}
+          keyboardType="numeric"
+          label="CPF/CNPJ"
+          value={doc}
+          onChangeText={(text) => setDoc(text)}
+        />
+        <TextInput
+          style={styles.input}
+          keyboardType="email-address"
+          label="E-mail"
+          value={mail}
+          onChangeText={(text) => setMail(text)}
+          leftIcon={{ type: 'font-awesome', name: 'chevron-left' }}
+        />
+        <TextInput
+          style={styles.input}
+          secureTextEntry
+          //onFocus={<TextInput.Icon icon="eye" />}
+          right={<TextInput.Icon icon="eye" />}
+          label="Senha"
+          value={senha}
+          onChangeText={(text) => setSenha(text)}
+        />
+        <Text style={styles.text}>Endereço</Text>
+        <ScrollView>
+          <TextInput
+            style={styles.input}            
+            keyboardType="numeric"
+            label="CEP"
+            value={cep}
+            onChangeText={(text) => setCep(text)}
+            onBlur={fetchAddress}
+          />
+          <TextInput
+            style={styles.input}
+            label="Rua"
+            value={rua}
+            onChangeText={(text) => setRua(text)}
+          />
+          <TextInput
+            style={styles.input}
+            keyboardType="numeric"
+            label="Número"
+            value={numero}
+            onChangeText={(text) => setNumero(text)}
+          />
+          <TextInput
+            style={styles.input}
+            label="Complemento"
+            value={complemento}
+            onChangeText={(text) => setComplemento(text)}
+          />
+          <TextInput
+            style={styles.input}
+            label="Bairro"
+            value={bairro}
+            onChangeText={(text) => setBairro(text)}
+          />
+          <TextInput
+            style={styles.input}
+            label="Cidade"
+            value={cidade}
+            onChangeText={(text) => setCidade(text)}
+          />
+          <TextInput
+            style={styles.input}
+            label="Estado"
+            value={estado}
+            onChangeText={(text) => setEstado(text)}
+          />
+          <TextInput
+            style={styles.input}            
+            keyboardType="phone-pad"
+            label="Telefone"
+            value={telefone}
+            onChangeText={(text) => setTelefone(text)}
+          />
+        </ScrollView>
+
+        <Button mode="contained" onPress={() => console.log('Pressed')}>
+          Cadastrar
+        </Button>
+        <Button mode="contained" color='red' onPress={() => navigation.goBack()}>
+          Voltar
+        </Button>
+      </ScrollView>
+    </ScrollView>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+  },
+  body: {
+    margin: 8,
+  },
+  input: {
+    backgroundColor: '#fff',
+    marginBottom: 8,
+    borderColor: "#ddd"
+    
+  },
+  titulo: {
+    textAlign: 'center',
+    fontSize: 24,
+    marginBottom: 8,
+  },
+  text: {
+    textAlign: 'left',
+    marginTop:32,  
+    fontSize: 16, 
+  },
+});
+
+export default Cadastro;
+
+
+// import { Text, Button } from 'react-native';
+
+// export default function Cadastrar({ navigation }) {
+//   return (
+//     <>
+//       <Text>Página de Cadastro</Text>
+//       <Button title="Voltar" onPress={() => navigation.goBack()}></Button>
+//     </>
+//   );
+// }
