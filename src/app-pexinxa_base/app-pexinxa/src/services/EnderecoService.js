3e256b86422486e555b57e0fc052266cd7e68209
Letícia Isabela <80654018+LeticiaIsabela@users.noreply.github.com>
2023-05-05 14:43:25 -0300

    Update EnderecoService.js

--- src/app-pexinxa_base/app-pexinxa/src/services/EnderecoService.js
+++ src/app-pexinxa_base/app-pexinxa/src/services/EnderecoService.js
@@ -1,1 +1,14 @@
+import Database from './DBServices.js';
 
+const DB_EXEC = Database.getConnection();
+
+export const getEndereco = async () => {
+  let results = await DB_EXEC(`select * from endereco`);
+  return results.rows._array;
+}
+
+export const insertEndereco = async (params) => {
+  let results = await DB_EXEC(`insert into endereco (rua, numero, bairro, cidade, estado, pais)
+  values(?,?,?,?)`, [params.rua, params.numero, params.bairro, params.cidade, params.estado, params.pais]);
+  return results.rowsAffected;
+}
