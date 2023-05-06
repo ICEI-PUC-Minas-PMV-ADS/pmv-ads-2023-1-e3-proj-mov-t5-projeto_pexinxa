a5a91c7e930ca1163d411eca9d78dedf4e4c6328
Letícia Isabela <80654018+LeticiaIsabela@users.noreply.github.com>
2023-05-06 11:50:34 -0300

    Update VendedorService.js

--- src/app-pexinxa_base/app-pexinxa/src/services/VendedorService.js
+++ src/app-pexinxa_base/app-pexinxa/src/services/VendedorService.js
@@ -7,8 +7,19 @@ export const getVendedor = async () => {
   return results.rows._array;
 }
 
-export const insertVendedor = async (params) => {
-  let results = await DB_EXEC(`insert into vendedor (cadastro, endereco)
-  values(?,?,?,?)`, [params.cadastro, params.endereco]);
+export const insertVendedor = async (param) => {
+  let results = await DB_EXEC(`insert into vendedor (cadastro)
+  values(?,?,?,?)`, [param.cadastro]);
+  return results.rowsAffected;
+}
+
+export const updateVendedor = async (param) => {
+  let results = await DB_EXEC(`update vendedor set cadastro=?
+  where id=?`, [param.cadastro]);
+  return results.rowsAffected;
+}
+
+export const deleteVendedor = async (id) => {
+  let results = await DB_EXEC(`delete from vendedor where id=?`, [id]);
   return results.rowsAffected;
 }
