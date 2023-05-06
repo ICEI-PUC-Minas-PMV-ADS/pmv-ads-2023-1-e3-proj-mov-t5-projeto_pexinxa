ec6e245dcc73848b6f2bf171df29d265c1082e2f
Letícia Isabela <80654018+LeticiaIsabela@users.noreply.github.com>
2023-05-06 11:47:12 -0300

    Update ProdutoService.js

--- src/app-pexinxa_base/app-pexinxa/src/services/ProdutoService.js
+++ src/app-pexinxa_base/app-pexinxa/src/services/ProdutoService.js
@@ -7,8 +7,19 @@ export const getProduto = async () => {
   return results.rows._array;
 }
 
-export const insertProduto = async (params) => {
+export const insertProduto = async (param) => {
   let results = await DB_EXEC(`insert into produto (nome, descricao, preco, imagem, categoria, quantidade, data_cadastro, vendedor)
-  values(?,?,?,?)`, [params.nome, params.descricao, params.preco, params.imagem, params.categoria, params.quantidade, params.data_cadastro, params.vendedor]);
+  values(?,?,?,?)`, [param.nome, param.descricao, param.preco, param.imagem, param.categoria, param.quantidade, param.data_cadastro, param.vendedor]);
+  return results.rowsAffected;
+}
+
+export const updateProduto = async (param) => {
+  let results = await DB_EXEC(`update produto set nome=?, descricao=?, preco=?, imagem=?, categoria=?, quantidade=?, data_cadastro=?, vendedor=?)
+  where=id?`, [param.nome, param.descricao, param.preco, param.imagem, param.categoria, param.quantidade, param.data_cadastro, param.vendedor]);
+  return results.rowsAffected;
+}
+
+export const deleteProduto = async (id) => {
+  let results = await DB_EXEC(`delete from gastos where id=?`, [id]);
   return results.rowsAffected;
 }
