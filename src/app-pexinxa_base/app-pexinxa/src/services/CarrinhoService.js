38ab2233e3851477e5b2f45b330415314041872a
Letícia Isabela <80654018+LeticiaIsabela@users.noreply.github.com>
2023-05-06 11:38:41 -0300

    Update CarrinhoService.js

--- src/app-pexinxa_base/app-pexinxa/src/services/CarrinhoService.js
+++ src/app-pexinxa_base/app-pexinxa/src/services/CarrinhoService.js
@@ -7,8 +7,19 @@ export const getCarrinho = async () => {
   return results.rows._array;
 }
 
-export const insertCarrinho = async (params) => {
+export const insertCarrinho = async (param) => {
   let results = await DB_EXEC(`insert into carrinho (produto, preco, quantidade_produto, data_criacao, vendedor)
-  values(?,?,?,?)`, [params.produto, params.preco, params.quantidade_produto, params.data_criacao, params.vendedor]);
+  values(?,?,?,?)`, [param.produto, param.preco, param.quantidade_produto, param.data_criacao, param.vendedor]);
+  return results.rowsAffected;
+}
+
+export const updateCarrinho = async (param) => {
+  let results = await DB_EXEC(`update carrinho produto=?, preco=?, quantidade_produto=?, data_criacao=?, vendedor=?)
+  where id=?`, [param.produto, param.preco, param.quantidade_produto, param.data_criacao, param.vendedor]);
+  return results.rowsAffected;
+}
+
+export const deleteCarrinho = async (id) => {
+  let results = await DB_EXEC(`delete from gastos where id=?`, [id]);
   return results.rowsAffected;
 }
