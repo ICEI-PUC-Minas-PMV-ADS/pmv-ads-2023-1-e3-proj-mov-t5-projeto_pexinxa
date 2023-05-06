79173c734749a8465838325e1197fd2f25655520
Letícia Isabela <80654018+LeticiaIsabela@users.noreply.github.com>
2023-05-06 11:42:40 -0300

    Update ConsumidorService.js

--- src/app-pexinxa_base/app-pexinxa/src/services/ConsumidorService.js
+++ src/app-pexinxa_base/app-pexinxa/src/services/ConsumidorService.js
@@ -3,12 +3,23 @@ import Database from './DBServices.js';
 const DB_EXEC = Database.getConnection();
 
 export const getConsumidor = async () => {
-  let results = await DB_EXEC(`select * from vendedor`);
+  let results = await DB_EXEC(`select * from consumidor`);
   return results.rows._array;
 }
 
-export const insertConsumidor = async (params) => {
-  let results = await DB_EXEC(`insert into consumidor (cadastro, endereco)
-  values(?,?,?,?)`, [params.cadastro, params.endereco]);
+export const insertConsumidor = async (param) => {
+  let results = await DB_EXEC(`insert into consumidor (cadastro)
+  values(?,?,?,?)`, [param.cadastro]);
+  return results.rowsAffected;
+}
+
+export const updateConsumidor = async (param) => {
+  let results = await DB_EXEC(`update consumidor cadastro=?
+  where id=?`, [param.cadastro]);
+  return results.rowsAffected;
+}
+
+export const deleteConsumidor = async (id) => {
+  let results = await DB_EXEC(`delete from consumidor where id=?`, [id]);
   return results.rowsAffected;
 }
