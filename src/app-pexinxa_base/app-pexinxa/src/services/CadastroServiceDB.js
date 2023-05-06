a8e97afd6ecb548713951af3eb4887bcb1220df4
Letícia Isabela <80654018+LeticiaIsabela@users.noreply.github.com>
2023-05-06 11:35:45 -0300

    Update CadastroServiceDB.js

--- src/app-pexinxa_base/app-pexinxa/src/services/CadastroServiceDB.js
+++ src/app-pexinxa_base/app-pexinxa/src/services/CadastroServiceDB.js
@@ -7,8 +7,19 @@ export const getCadastro = async () => {
   return results.rows._array;
 }
 
-export const insertCadastro = async (params) => {
+export const insertCadastro = async (param) => {
   let results = await DB_EXEC(`insert into cadastro (nome, doc, mail, senha, cep, rua, numero, complemento, bairro, cidade, estado, telefone)
-  values(?,?,?,?)`, [params.nome, params.doc, params.mail, params.senha, params.dep, params.rua, params.numero, params.complemento, params.bairro, params.cidade, params.estado, params.telefone]);
+  values(?,?,?,?)`, [param.nome, param.doc, param.mail, param.senha, param.dep, param.rua, param.numero, param.complemento, param.bairro, param.cidade, param.estado, param.telefone]);
+  return results.rowsAffected;
+}
+
+export const updateCadastro = async (param) => {
+  let results = await DB_EXEC(`update cadastro set nome=?, doc=?, mail=?, senha=?, cep=?, rua=?, numero=?, complemento=?, bairro=?, cidade=?, estado=?, telefone=?
+  where id=?`, [param.nome, param.doc, param.mail, param.senha, param.dep, param.rua, param.numero, param.complemento, param.bairro, param.cidade, param.estado, param.telefone]);
+  return results.rowsAffected;
+}
+
+export const deleteCadastro = async (id) => {
+  let results = await DB_EXEC(`delete from gastos where id=?`, [id]);
   return results.rowsAffected;
 }
