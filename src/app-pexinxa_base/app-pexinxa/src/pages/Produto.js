882811cd295b324f4f8cbed00ed0e9ddcd4988c3
Letícia Isabela <80654018+LeticiaIsabela@users.noreply.github.com>
2023-05-07 10:50:47 -0300

    Update Produto.js

--- src/app-pexinxa_base/app-pexinxa/src/pages/Produto.js
+++ src/app-pexinxa_base/app-pexinxa/src/pages/Produto.js
@@ -3,7 +3,93 @@ import { CartContext } from '../context/cart-context';
 
 import { Text, View, Image, StyleSheet, TouchableOpacity } from 'react-native';
 
+import {insertProduto, updateProduto, deleteProduto, getProduto} from '../services/ProdutoService';
+import {useNavigation} from '@react-navigation/native';
+
+const Product1 = () => {
+  
+  const navigation = useNavigation();
+  
+  const [nome, setNome] = useState('');
+  const [descricao, setDescricao] = useState('');
+  const [preco, setPreco] = useState('');
+  const [imagem, setImagem] = useState('');
+  const [categoria, setCategoria] = useState('');
+  const [quantidade, setQuantidade] = useState('');
+  const [data_cadastro, setDataCadastro] = useState('');
+  const [vendedor, setVendedor] = useState('');
+
+  useEffect(() => {
+    if(item){
+      setNome(item.nome);
+      setDescricao(item.descricao);
+      setPreco(item.preco);
+      setImagem(item.imagem);
+      setCategoria(item.categoria);
+      setQuantidade(item.quantidade);
+      setData_cadastro(item.data_cadastro);
+      setVendedor(item.vendedor);
+    }
+  }, [item]);
+
+  const handleSalvar = () => {
+
+    if(item){
+
+      updateProduto(
+        {
+          nome: nome,
+          descricao: descricao,
+          preco: preco,
+          imagem: imagem,
+          categoria: categoria,
+          quantidade: quantidade,
+          data_cadastro: data_cadastro,
+          vendedor: vendedor,
+          id: item.id,
+        }).then();
+
+    } else {
+
+      insertProduto(
+        {
+
+          nome: nome,
+          descricao: descricao,
+          preco: preco,
+          imagem: imagem,
+          categoria: categoria,
+          quantidade: quantidade,
+          data_cadastro: data_cadastro,
+          vendedor: vendedor
+
+        }).then();
+
+    }
+
+    navigation.goBack();
+  };
+
+  const handleExcluir = () => {
+
+    deleteProduto(item.id).then();
+    navigation.goBack();
+  };
+}
+
 export default function Produto({ route, params }) {
+  
+  const navigation = useNavigation();
+
+  const [produto, setProduto] = useState([]);
+
+  useEffect(() => {
+    getProduto().then((dados) => {
+      setProduto(dados);
+    });
+  },[]);
+  
+  
   const { product } = route.params;
 
   const [state, dispatch] = useContext(CartContext);
